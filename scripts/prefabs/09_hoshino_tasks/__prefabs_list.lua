e764aeb1993bd35df6c5babec20d920f7b4bee79
unknown <under.dark.night@outlook.com>
2024-11-09 15:55:49 +0800

    white mission 40 finish

--- scripts/prefabs/09_hoshino_tasks/__prefabs_list.lua
+++ scripts/prefabs/09_hoshino_tasks/__prefabs_list.lua
@@ -71,6 +71,7 @@ local prefabs_name_list = {
     "white_37",                             --- 白色37
     "white_38",                             --- 白色38
     "white_39",                             --- 白色39
+    "white_40",                             --- 白色40
 
 
 }
