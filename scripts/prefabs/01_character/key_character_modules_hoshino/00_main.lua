807408031d6ed45eb573a8e93c5ba70663f0a144
unknown <under.dark.night@outlook.com>
2024-12-15 11:44:10 +0800

    remove heavy lifting module

--- scripts/prefabs/01_character/key_character_modules_hoshino/00_main.lua
+++ scripts/prefabs/01_character/key_character_modules_hoshino/00_main.lua
@@ -51,7 +51,7 @@ return function(inst)
         "prefabs/01_character/key_character_modules_hoshino/36_travel_traces_spawner",                  ---- 遍历之迹 刷新控制器
         "prefabs/01_character/key_character_modules_hoshino/37_anim_state_hook",                        ---- inst.AnimState hook
         "prefabs/01_character/key_character_modules_hoshino/38_new_selected_gift",                      ---- 新选礼物
-        "prefabs/01_character/key_character_modules_hoshino/39_heavylifting",                           ---- 搬重物 处理
+        -- "prefabs/01_character/key_character_modules_hoshino/39_heavylifting",                           ---- 搬重物 处理
         "prefabs/01_character/key_character_modules_hoshino/40_temperature_controller",                 ---- 温度控制
         "prefabs/01_character/key_character_modules_hoshino/41_sanity_controller",                      ---- Sanity 控制器
         "prefabs/01_character/key_character_modules_hoshino/42_halo_controller",                        ---- 头顶光环 控制器
