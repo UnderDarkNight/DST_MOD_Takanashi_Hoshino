cf07922d20ca65ac24dcf233059934a3d39f03bc
unknown <under.dark.night@outlook.com>
2025-01-08 13:16:17 +0800

    hoshino_com_task_sys_for_player kill event bug fix

--- key_modules_for_hoshino/23_mission_shop_level_sys_for_other_player/03_main_com_install.lua
+++ key_modules_for_hoshino/23_mission_shop_level_sys_for_other_player/03_main_com_install.lua
@@ -43,6 +43,15 @@ AddPlayerPostInit(function(inst)
         if inst.components.hoshino_com_task_sys_for_player == nil then
             inst:AddComponent("hoshino_com_task_sys_for_player")
         end
+        --- 经验广播触发监听
+        inst:ListenForEvent("hoshino_event.exp_broadcast",function(inst,_table)
+            local max_health = _table.max_health
+            local prefab = _table.prefab -- 暂时预留，给某些特殊经验爆表的怪。
+            local exp = max_health/10
+            inst.components.hoshino_com_task_sys_for_player:KillBroadcast(prefab,1,{
+                exp = exp
+            })
+        end)
     ------------------------------------------------------------------------
     --- 商店系统+货币系统
         if inst.components.hoshino_com_shop == nil then
--- scripts/prefabs/01_character/key_character_modules_hoshino/34_task_sys_install.lua
+++ scripts/prefabs/01_character/key_character_modules_hoshino/34_task_sys_install.lua
@@ -16,15 +16,15 @@ return function(inst)
     end
 
     ----------------------------------------------------------------------------------------------------------------------------------------------------------
-    --- 经验广播触发监听
-        inst:ListenForEvent("hoshino_event.exp_broadcast",function(inst,_table)
-            local max_health = _table.max_health
-            local prefab = _table.prefab -- 暂时预留，给某些特殊经验爆表的怪。
-            local exp = max_health/10
-            inst.components.hoshino_com_task_sys_for_player:KillBroadcast(prefab,1,{
-                exp = exp
-            })
-        end)
-----------------------------------------------------------------------------------------------------------------------------------------------------------
+    --- 经验广播触发监听 -- 【笔记】已经移动到 所有玩家的通用模块处
+        -- inst:ListenForEvent("hoshino_event.exp_broadcast",function(inst,_table)
+        --     local max_health = _table.max_health
+        --     local prefab = _table.prefab -- 暂时预留，给某些特殊经验爆表的怪。
+        --     local exp = max_health/10
+        --     inst.components.hoshino_com_task_sys_for_player:KillBroadcast(prefab,1,{
+        --         exp = exp
+        --     })
+        -- end)
+    ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 end
