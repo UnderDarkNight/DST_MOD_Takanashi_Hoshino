b59e7ff4a27cc6d4ab3a1e2b4b3fce4a121c426a
Lidievable <lyqpxyqxp@qq.com>
2025-01-24 00:01:33 +0800

    artifact 天 cd调整

--- scripts/prefabs/02_items/36_artifact_sky.lua
+++ scripts/prefabs/02_items/36_artifact_sky.lua
@@ -22,7 +22,7 @@ Artifact 天
 ----------------------------------------------------------------------------------------------------------------------------------------------------
 -- 参数
     local SPELL_ACTIVE_RADIUS = 6  --- 施法范围
-    local MAX_COOL_DOWN_TIME = 10
+    local MAX_COOL_DOWN_TIME = 15
     --- 怪物扫描的tag ，复制自 亮茄魔杖
     local BOUNCE_MUST_TAGS = { "_combat" }
     local BOUNCE_NO_TAGS = { "INLIMBO", "wall", "notarget", "player", "companion", "flight", "invisible", "noattack", "hiding" }
