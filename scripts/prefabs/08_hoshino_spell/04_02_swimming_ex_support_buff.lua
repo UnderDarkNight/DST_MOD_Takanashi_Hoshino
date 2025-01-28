00ca593aeb40967cf7cc84197016f658877455cb
Lidievable <lyqpxyqxp@qq.com>
2025-01-28 15:12:40 +0800

    版本更新

--- scripts/prefabs/08_hoshino_spell/04_02_swimming_ex_support_buff.lua
+++ scripts/prefabs/08_hoshino_spell/04_02_swimming_ex_support_buff.lua
@@ -52,7 +52,7 @@ local function OnAttached(inst,target) -- 玩家得到 debuff 的瞬间。 穿�
     --- 给玩家上buff
         inst:DoPeriodicTask(1,function()
             local player_level = target.components.hoshino_com_level_sys:GetLevel()
-            local heal_num = 3 +player_level * 0.05
+            local heal_num = 1 +player_level * 0.05
             local speed_mult = 1.3
             local damage_mult = (30+player_level*1)/100 + 1
             local cost_value_num = 0.03 + player_level*0.003
