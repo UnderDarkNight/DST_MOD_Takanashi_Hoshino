9dd8d89e8afdd3928aabe19252575ef7cf7e20ab
unknown <under.dark.night@outlook.com>
2025-03-13 13:05:26 +0800

    hoshino_card_debuff_road_of_pain

--- scripts/prefabs/04_debuffs/__prefabs_list.lua
+++ scripts/prefabs/04_debuffs/__prefabs_list.lua
@@ -54,6 +54,7 @@ local prefabs_name_list = {
     "01_24_card_debuff_weapon_dmg_up_by_range",                             --- 卡牌debuff：武器伤害随距离增加
     "01_25_card_debuff_armored_warrior",                                    --- 卡牌debuff：重装战士
     "01_26_card_debuff_moisture_and_dmg",                                   --- 卡牌debuff：湿度和伤害
+    "01_27_card_debuff_road_of_pain",                                       --- 卡牌debuff：痛苦之路
 
 
     "02_monster_damage_down_debuff",                         --- 怪物伤害倍增器。
