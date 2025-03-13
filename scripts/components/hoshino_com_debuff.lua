7a2de0b395f70461c6044b6447d6a8dc5df1f79c
unknown <under.dark.night@outlook.com>
2025-03-13 16:55:58 +0800

    hoshino_com_debuff upgrade

--- scripts/components/hoshino_com_debuff.lua
+++ scripts/components/hoshino_com_debuff.lua
@@ -33,48 +33,44 @@
     local function hook_components(self,inst)
         --------------------------------------------------------------------------------
         --- 血量上限
-            local value_temp_inst = CreateEntity()
-            inst:ListenForEvent("onremove",function()
-                value_temp_inst:Remove()
-            end)
             function self:Add_Max_Helth(value)
-                inst.components.hoshino_com_max_value_controller:AddTempExtraHealth(value_temp_inst,value)
+                inst.components.hoshino_com_max_value_controller:AddTempExtraHealth(GetSpeedMultInst(self),value)
             end
             self:AddOnLoadFn(function()
                 local max_health = self:Get("max_health")
                 if max_health then
-                    inst.components.hoshino_com_max_value_controller:AddTempExtraHealth(value_temp_inst,max_health)
+                    inst.components.hoshino_com_max_value_controller:AddTempExtraHealth(GetSpeedMultInst(self),max_health)
                     self:Set("max_health",nil)
                 end
             end)
         --------------------------------------------------------------------------------
         --- San上限
             function self:Add_Max_Sanity(value)
-                inst.components.hoshino_com_max_value_controller:AddTempExtraSanity(value_temp_inst,value)
+                inst.components.hoshino_com_max_value_controller:AddTempExtraSanity(GetSpeedMultInst(self),value)
             end
             self:AddOnLoadFn(function()
                 local max_sanity = self:Get("max_sanity")
                 if max_sanity then
-                    inst.components.hoshino_com_max_value_controller:AddTempExtraSanity(value_temp_inst,max_sanity)
+                    inst.components.hoshino_com_max_value_controller:AddTempExtraSanity(GetSpeedMultInst(self),max_sanity)
                     self:Set("max_sanity",nil)
                 end
             end)
         --------------------------------------------------------------------------------
         --- hunger 上限
             function self:Add_Max_Hunger(value)
-                inst.components.hoshino_com_max_value_controller:AddTempExtraHunger(value_temp_inst,value)
+                inst.components.hoshino_com_max_value_controller:AddTempExtraHunger(GetSpeedMultInst(self),value)
             end
             self:AddOnLoadFn(function()
                 local max_hunger = self:Get("max_hunger")
                 if max_hunger then
-                    inst.components.hoshino_com_max_value_controller:AddTempExtraHunger(value_temp_inst,max_hunger)
+                    inst.components.hoshino_com_max_value_controller:AddTempExtraHunger(GetSpeedMultInst(self),max_hunger)
                     self:Set("max_hunger",nil)
                 end
             end)
         --------------------------------------------------------------------------------
         --- 移动速度加成
             function self:Add_Speed_Mult(value)
-                local speed_mult = self:Add("speed_mult",value) + 1
+                local speed_mult = math.max(self:Add("speed_mult",value) + 1,0.01)
                 inst.components.locomotor:SetExternalSpeedMultiplier(GetSpeedMultInst(self), "hoshino_com_debuff_speed_mult",speed_mult)
             end
             self:AddOnLoadFn(function()
@@ -223,7 +219,7 @@
         --------------------------------------------------------------------------------
         -- 位面防御
             function self:Add_Planar_Defense(value)
-                self:Add("planar_defense_value",value)
+                self:Add("planar_defense_value",value,0,1000000000000000000)
                 self.inst:PushEvent("hoshino_other_armor_item_param_refresh")
             end
             function self:Get_Planar_Defense()
@@ -330,12 +326,11 @@
     end
 ------------------------------------------------------------------------------------------------------------------------------
 ----- 数据读取/储存
-
-    function hoshino_com_debuff:Get(index)
+    function hoshino_com_debuff:Get(index,default)
         if index then
-            return self.DataTable[index]
+            return self.DataTable[index] or default
         end
-        return nil
+        return default
     end
     function hoshino_com_debuff:Set(index,theData)
         if index then
@@ -343,10 +338,15 @@
         end
     end
 
-    function hoshino_com_debuff:Add(index,num)
+    function hoshino_com_debuff:Add(index,num,min,max)
         if index then
-            self.DataTable[index] = (self.DataTable[index] or 0) + ( num or 0 )
-            return self.DataTable[index]
+            if max == nil and min == nil then
+                self.DataTable[index] = (self.DataTable[index] or 0) + ( num or 0 )
+                return self.DataTable[index]
+            elseif type(max) == "number" and type(min) == "number" then
+                self.DataTable[index] = math.clamp( (self.DataTable[index] or 0) + ( num or 0 ) , min , max )
+                return self.DataTable[index]
+            end                    
         end
         return 0
     end
--- test_fn/test.lua
+++ test_fn/test.lua
@@ -26,27 +26,27 @@ local flg,error_code = pcall(function()
         end
     ----------------------------------------------------------------------------------------------------------------    ----------------------------------------------------------------------------------------------------------------
     --- 卡牌调试
-        local item = SpawnPrefab("hoshino_item_cards_pack")
-        item:PushEvent("Set",{
-                cards = {
-        --             -- "card_golden",
-        --             -- "card_white",
-        --             -- "card_colourful",
-        --             -- "card_colourful",
-        --             -- "card_golden",
-        --             -- "card_black",
-                    "mark_moon_land_and_ancient_land",
-        --             -- "unlock_spell_normal_ex",
-        --             -- "unlock_spell_swimming_ex",
-        --             -- "unlock_spell_all_normal",
-        --             -- "unlock_spell_all_swimming",
-        --             -- "kill_and_explode",
-        --             -- "give_me_some_money",
-        --             "level_up_and_double_card_pack",
-                },
-            }
-        )
-        ThePlayer.components.inventory:GiveItem(item)
+        -- local item = SpawnPrefab("hoshino_item_cards_pack")
+        -- item:PushEvent("Set",{
+        --         cards = {
+        -- --             -- "card_golden",
+        -- --             -- "card_white",
+        -- --             -- "card_colourful",
+        -- --             -- "card_colourful",
+        -- --             -- "card_golden",
+        -- --             -- "card_black",
+        --             "mark_moon_land_and_ancient_land",
+        -- --             -- "unlock_spell_normal_ex",
+        -- --             -- "unlock_spell_swimming_ex",
+        -- --             -- "unlock_spell_all_normal",
+        -- --             -- "unlock_spell_all_swimming",
+        -- --             -- "kill_and_explode",
+        -- --             -- "give_me_some_money",
+        -- --             "level_up_and_double_card_pack",
+        --         },
+        --     }
+        -- )
+        -- ThePlayer.components.inventory:GiveItem(item)
     ----------------------------------------------------------------------------------------------------------------
     --- 调试任务栏
         -- ThePlayer.___task_board_widget_fn = function(inst,front_root)
