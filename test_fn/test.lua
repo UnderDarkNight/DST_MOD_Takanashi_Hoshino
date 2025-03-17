1b3cba2ccd9e717b82c933c262659953fa41fca4
Lidievable <lyqpxyqxp@qq.com>
2025-03-17 17:20:13 +0800

    文案修改

--- test_fn/test.lua
+++ test_fn/test.lua
@@ -27,9 +27,9 @@ local flg,error_code = pcall(function()
         
     ----------------------------------------------------------------------------------------------------------------    ----------------------------------------------------------------------------------------------------------------
     --- 卡牌调试
-        -- local item = SpawnPrefab("hoshino_item_cards_pack")
-        -- item:PushEvent("Set",{
-        --         cards = {
+        local item = SpawnPrefab("hoshino_item_cards_pack")
+        item:PushEvent("Set",{
+                cards = {
         --             -- "card_golden",
         --             -- "card_white",
         -- --             -- "card_colourful",
@@ -39,7 +39,7 @@ local flg,error_code = pcall(function()
         --             -- "card_black",
         --             -- "card_colourful",
 
-        --             "sacrifice",
+                    "chaos",
         -- --             -- "unlock_spell_normal_ex",
         -- --             -- "unlock_spell_swimming_ex",
         -- --             -- "unlock_spell_all_normal",
@@ -47,10 +47,10 @@ local flg,error_code = pcall(function()
         -- --             -- "kill_and_explode",
         -- --             -- "give_me_some_money",
         -- --             "level_up_and_double_card_pack",
-        --         },
-        --     }
-        -- )
-        -- ThePlayer.components.inventory:GiveItem(item)
+                },
+            }
+        )
+        ThePlayer.components.inventory:GiveItem(item)
     ----------------------------------------------------------------------------------------------------------------
     --- 调试任务栏
         -- ThePlayer.___task_board_widget_fn = function(inst,front_root)
@@ -60,7 +60,7 @@ local flg,error_code = pcall(function()
 
         -- print(TheWorld.Map:GetTileAtPoint(x,y,z))
 
-        local box = TheSim:FindFirstEntityWithTag("set_the_same_followers")
+        -- local box = TheSim:FindFirstEntityWithTag("set_the_same_followers")
         -- box.components.container:GiveItem(SpawnPrefab("hoshino_mission_white_12"))
         -- box.components.hoshino_com_task_sys_for_building:Debug_Set_Mission("hoshino_mission_colourful_12")
         -- box.components.hoshino_com_task_sys_for_building:Debug_Set_Mission("hoshino_mission_golden_31")
@@ -86,7 +86,7 @@ local flg,error_code = pcall(function()
         -- for i = 1, 30, 1 do
         --     ThePlayer:AddDebuff("hoshino_card_debuff_void_throat","hoshino_card_debuff_void_throat")
         -- end
-        ThePlayer:AddDebuff("hoshino_debuff_bomb_shield","hoshino_debuff_bomb_shield")
+        -- ThePlayer:AddDebuff("hoshino_debuff_bomb_shield","hoshino_debuff_bomb_shield")
     ----------------------------------------------------------------------------------------------------------------
     print("WARNING:PCALL END   +++++++++++++++++++++++++++++++++++++++++++++++++")
 end)
